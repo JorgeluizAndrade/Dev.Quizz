99d068e51efd339b4dca46e8fcf8545c92b2c97f
jorge luiz <luizj1718@gmail.com>
2025-02-24 12:47:04 -0300

    feat select compoment for facilitate user input

--- src/components/QuizCreation.tsx
+++ src/components/QuizCreation.tsx
@@ -8,6 +8,8 @@ import {
   CardHeader,
   Divider,
   Input,
+  Select,
+  SelectItem,
 } from "@nextui-org/react";
 import { BookOpen, CopyCheck } from "lucide-react";
 import { useRouter } from "next/navigation";
@@ -19,6 +21,7 @@ import axios, { AxiosError } from "axios";
 import { MutationCache, useMutation } from "@tanstack/react-query";
 import LoadingRadar from "./LoadingRadar";
 import { toast } from "react-toastify";
+import { tech } from "@/data/tech";
 
 type Props = {
   topic: string;
@@ -35,7 +38,7 @@ const QuizCreation = ({ topic: topicParam }: Props) => {
       const response = await axios.post("/api/game", { amount, topic, type });
       return response.data;
     },
-  }, );
+  });
 
   const validateTopic = (tpc: string) => {
     return true;
@@ -66,32 +69,32 @@ const QuizCreation = ({ topic: topicParam }: Props) => {
         type: input.type,
       },
       {
-        onSuccess: ({ gameId }: { gameId: string })  => {
+        onSuccess: ({ gameId }: { gameId: string }) => {
           setFinishedLoading(true);
-       const timeOut = setTimeout(()=> {
+          const timeOut = setTimeout(() => {
             if (form.getValues("type") === "open_ended") {
               router.push(`/play/open-ended/${gameId}`);
             } else if (form.getValues("type") === "mcq") {
               router.push(`/play/mcq/${gameId}`);
             }
-            }, 2000);
-            return () => clearTimeout(timeOut);
-        },  
+          }, 2000);
+          return () => clearTimeout(timeOut);
+        },
         onError: (error) => {
-          setShowLoader(false)
-          if(error instanceof AxiosError){
-              if(error.response?.status === 500){
-                toast('Something went wrong', {
-                  position: "top-center",
-                  autoClose: 5000,
-                  hideProgressBar: false,
-                  closeOnClick: true,
-                  pauseOnHover: true,
-                  draggable: true,
-                  progress: undefined,
-                  theme: "light",
-                  });
-              }
+          setShowLoader(false);
+          if (error instanceof AxiosError) {
+            if (error.response?.status === 500) {
+              toast("Something went wrong", {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+              });
+            }
           }
         },
       }
@@ -100,8 +103,9 @@ const QuizCreation = ({ topic: topicParam }: Props) => {
 
   form.watch();
 
-  if(showLoader){
-    return <LoadingRadar finished={finishedLoading} /> 
+
+  if (showLoader) {
+    return <LoadingRadar finished={finishedLoading} />;
   }
 
   return (
@@ -113,28 +117,40 @@ const QuizCreation = ({ topic: topicParam }: Props) => {
         </CardHeader>
         <Divider />
         <CardBody>
-          <div className="flex flex-col gap-2 items-center justify-center  overflow-auto">
+          <div className="flex flex-col gap-2 items-center justify-center overflow-auto">
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
               <div className="flex flex-col mt-4 w-full max-w-xs gap-4">
                 <Controller
                   name="topic"
                   control={form.control}
                   render={({ field }) => (
-                    <Input
-                      label="Topic"
-                      placeholder="Enter a topic"
-                      color={isTopicInvalid(field.value) ? "danger" : "success"}
-                      errorMessage={
-                        isTopicInvalid(field.value) &&
-                        "Topic must be at least 4 characters long"
-                      }
-                      maxLength={15}
-                      variant="bordered"
-                      description="Please provide any topic you would like to be quizzed on here."
-                      {...field}
-                    />
+                    <Select
+                    {...field}
+                      isRequired 
+                      name="topic"
+                      variant="underlined"
+                      className="max-w-xs"
+                      label="Enter a topic"
+                      placeholder="Programming Language or Framework "
+                    >
+                      {tech.map((tech) => (
+                        <SelectItem key={tech.key}>{tech.label}</SelectItem>
+                      ))}
+                      
+                    </Select>
                   )}
                 />
+
+                {/* <Select
+                  isRequired
+                  className="max-w-xs"
+                  label="Enter a topic"
+                  placeholder="Programming Language or Framework "
+                >
+                  {tech.map((animal) => (
+                    <SelectItem key={animal.key}>{animal.label}</SelectItem>
+                  ))}
+                </Select> */}
 
                 <Input
                   {...form.control}
@@ -156,7 +172,9 @@ const QuizCreation = ({ topic: topicParam }: Props) => {
                 render={({ field }) => (
                   <div className="flex justify-between">
                     <Button
-                      color={ form.getValues("type") === "mcq" ?  "success" : "default"}
+                      color={
+                        form.getValues("type") === "mcq" ? "success" : "default"
+                      }
                       className="w-1/2 rounded-none rounded-l-lg"
                       onClick={() => {
                         form.setValue("type", "mcq");
@@ -168,7 +186,9 @@ const QuizCreation = ({ topic: topicParam }: Props) => {
                     <Divider orientation="vertical" />
                     <Button
                       color={
-                        form.getValues("type") === "open_ended" ? "success" : "default"
+                        form.getValues("type") === "open_ended"
+                          ? "success"
+                          : "default"
                       }
                       className="w-1/2 rounded-none rounded-r-lg"
                       onClick={() => form.setValue("type", "open_ended")}
