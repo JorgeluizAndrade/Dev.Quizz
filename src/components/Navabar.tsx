1c59beba3ee9ef90264dbdf5a3cb9a1a1024696a
jorge luiz <luizj1718@gmail.com>
2025-02-26 20:47:28 -0300

    change logo

--- src/components/Navabar.tsx
+++ src/components/Navabar.tsx
@@ -4,6 +4,7 @@ import DevQuizzLogo from "./Ui/DevQuizzLogo";
 import SignInButton from "./SignInButton";
 import UserAccount from "./UserAccount";
 import ThemeToggle from "./ThemeToggle";
+import Link from "next/link";
 
 
 type Props = {};
@@ -11,24 +12,19 @@ type Props = {};
 const Navabar = async (props: Props) => {
   const session = await getAuthSession();
   return (
-    <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-50 h-fit border-b border-zinc-300 py-2 ">
-      <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl">
+    <header className="fixed inset-x-0 top-0 z-50 h-16 bg-white dark:bg-black border-b border-zinc-300 dark:border-zinc-700">
+    <nav className="flex items-center justify-between h-full px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <Link href="/" className="flex items-center space-x-2">
         <DevQuizzLogo />
-        
-        <div className="flex items-center">
-          <ThemeToggle className="mr-4" />
-          <div className="flex items-center">
-            {session?.user ? (
-              <h1>
-                <UserAccount user={session.user} />
-              </h1>
-            ) : (
-              <SignInButton text="Sign in" />
-            )}
-          </div>
-        </div>
+        <span className="text-base font-bold text-gray-900 dark:text-white">DevQuizz</span>
+      </Link>
+
+      <div className="flex items-center space-x-4">
+        <ThemeToggle />
+        {session?.user ? <UserAccount user={session.user} /> : <SignInButton text="Sign in" />}
       </div>
-    </div>
+    </nav>
+  </header>
   );
 };
 
