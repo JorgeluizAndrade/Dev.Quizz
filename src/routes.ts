42907b98c73b58f0f0531f9042211046243f14fa
jorge luiz <luizj1718@gmail.com>
2025-03-24 12:36:52 -0300

    feat: routes for middleware

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+export const publicRoutes = [
+    "/",
+    "/education",
+    "/education/:slug",
+  ];
+  
+  export const authRoutes = ["/", "/error"];
+  
+  export const apiAuthPrefix = "/api/";
+  
+  export const DEFAULT_LOGIN_REDIRECT = "/dashboard";
