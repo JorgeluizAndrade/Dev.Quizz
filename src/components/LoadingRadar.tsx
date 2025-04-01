14d894ce802e009d236fde06431f9d393694d912
jorge luiz <luizj1718@gmail.com>
2025-04-01 12:46:25 -0300

    fix: bug design because of Footer

--- src/components/LoadingRadar.tsx
+++ src/components/LoadingRadar.tsx
@@ -26,7 +26,7 @@ const LoadingRadar = ({ finished }: Props) => {
   })
 
   return (
-    <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-[70vw] md:w-[60vw] flex flex-col items-center">
+    <div className=" justify-center  min-h-[calc(100vh-16rem)] w-[70vw] md:w-[60vw] flex flex-col items-center">
       <Image src={'/lo-fi-concept-animate.svg'} alt='loading' width={400} height={400}  />
       <h1 className="mt-2 text-xl">{loadingText}</h1>
     </div>
